971bcd08670a07fbcfcaaa978d70e72820595855
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-20 21:22:32 +0600

    heading text animation is done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerate";
 
 function Hero() {
   return (
@@ -14,13 +15,23 @@ function Hero() {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="white" />
       </div>
-      <div className="h-screen absolute top-0 left-0 w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center">
+      <div className="h-screen absolute top-0 left-0 w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.01] bg-grid-black/[0.2]  flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-blue-100 max-w-80">dynamic web magic with nextjs</h2>
+          <h2 className="uppercase tracking-widest text-xs text-blue-100 max-w-80">
+            dynamic web magic with nextjs
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless experiences"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Md Anisur Rahman, a Backend Developer based in
+            Bangladesh
+          </p>
         </div>
       </div>
     </div>
